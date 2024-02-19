86e7095077f5602bcbf81b5872ff3c5705c7ba02
CrazyProger1 <crazyproger1@gmail.com>
2024-02-19 13:43:46 +0200

    Add: constant header on scrolling top

--- client/src/components/headers/Header.jsx
+++ client/src/components/headers/Header.jsx
@@ -1,5 +1,50 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import {Navbar} from "react-bootstrap";
 
-const Header = () => <div/>
+const Header = () => {
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [visible, setVisible] = useState(true);
+    const [headerHeight, setHeaderHeight] = useState(0);
+    const headerRef = useRef(null);
+
+    useEffect(() => {
+        setHeaderHeight(headerRef.current ? headerRef.current.clientHeight : 0);
+    }, [headerRef])
+
+    const handleScroll = () => {
+        const currentScrollPos = window.pageYOffset;
+        const isScrolledDown = prevScrollPos < currentScrollPos;
+
+        setVisible(!isScrolledDown);
+        setPrevScrollPos(currentScrollPos);
+    };
+
+
+    const handleResize = () =>
+        setHeaderHeight(headerRef.current ? headerRef.current.clientHeight : 0);
+
+
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleResize);
+
+    return (
+        <div>
+            <Navbar
+                ref={headerRef}
+                expand='lg'
+                bg='dark'
+                data-bs-theme='dark'
+                style={{
+                    top: visible ? '0' : `-${headerHeight}px`
+                }}
+                collapseOnSelect>
+
+            </Navbar>
+            <div style={{
+                height: headerHeight
+            }}/>
+        </div>
+    );
+};
 
 export default Header;
