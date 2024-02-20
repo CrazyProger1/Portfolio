7b9a497cad944e2121a20bec3309410545889c9c
CrazyProger1 <crazyproger1@gmail.com>
2024-02-20 19:32:38 +0200

    Fix: header nav

--- client/src/components/headers/Header.jsx
+++ client/src/components/headers/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {Container, Nav, Navbar} from 'react-bootstrap';
-import {FaGithub} from "react-icons/fa";
-import useScrollNavigate from '../../hooks/useScrollNavigate';
-import {GITHUB_LINK} from "../../config/data";
+import {FaGithub, FaLinkedin, FaTelegram} from "react-icons/fa";
+import {GITHUB_LINK, LINKEDIN_LINK, TELEGRAM_LINK} from "../../config/data";
 import {HEADER_NAV_LINKS} from "../../config/constants";
+import useScrollNavigate from '../../hooks/useScrollNavigate';
 import '../../styles/Header.css'
 
 const Header = () => {
@@ -14,31 +14,38 @@ const Header = () => {
             <Navbar
                 className='header'
                 expand='lg'
-                bg='dark'
+                // bg='dark'
                 data-bs-theme='dark'
                 collapseOnSelect>
 
                 <Container>
-                    <Navbar.Brand>crazyproger1</Navbar.Brand>
+                    <Navbar.Brand className='header-brand'>crazyproger1</Navbar.Brand>
                     <Navbar.Toggle aria-controls='header-navbar'/>
                     <Navbar.Collapse id='header-navbar'>
                         <Nav className='mx-auto'>
                             {HEADER_NAV_LINKS.map(({text, to}) =>
                                 <Nav.Link
-                                    className='header-nav-link'
-                                    onClick={() => {
-                                        navigate(to);
-                                    }}>
+                                    className='header-nav-link ms-3'
+                                    onClick={() => navigate(to)}>
                                     {text}
                                 </Nav.Link>
                             )}
                         </Nav>
                     </Navbar.Collapse>
-                    <Nav.Link href={GITHUB_LINK} target='_blank'>
-                        <FaGithub color='white' size='32px'/>
-                    </Nav.Link>
-                </Container>
+                    <Nav>
+                        <Nav.Link href={TELEGRAM_LINK} target='_blank'>
+                            <FaTelegram color='white' size='32px'/>
+                        </Nav.Link>
 
+                        <Nav.Link href={LINKEDIN_LINK} target='_blank'>
+                            <FaLinkedin color='white' size='32px'/>
+                        </Nav.Link>
+
+                        <Nav.Link  href={GITHUB_LINK} target='_blank'>
+                            <FaGithub color='white' size='32px'/>
+                        </Nav.Link>
+                    </Nav>
+                </Container>
             </Navbar>
         </div>
     );
--- client/src/config/data.js
+++ client/src/config/data.js
@@ -1,1 +1,3 @@
-export const GITHUB_LINK = 'https://github.com/CrazyProger1'
+export const GITHUB_LINK = 'https://github.com/CrazyProger1';
+export const LINKEDIN_LINK = '';
+export const TELEGRAM_LINK = '';
