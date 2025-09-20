bb5b916a64c427e099a333f1e163b439df44cf0c
CrazyProger1 <crazyproger1@gmail.com>
2025-09-20 13:02:13 +0300

    add: some migrations

--- backend/src/apps/portfolio/models/projects.py
+++ backend/src/apps/portfolio/models/projects.py
@@ -29,6 +29,9 @@ class UserProject(models.Model):
         blank=False,
     )
 
+    def __str__(self):
+        return self.project.name
+
 
 class Project(CreatedAtMixin, UpdatedAtMixin, models.Model):
     users = models.ManyToManyField(
