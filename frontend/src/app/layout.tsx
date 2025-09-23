80315a77438a25d07510a1db33fab2422e457763
CrazyProger1 <crazyproger1@gmail.com>
2025-09-23 17:34:19 +0300

    fix: fonts

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Poppins } from "next/font/google";
 import { ReactNode } from "react";
 
 import { ModalProvider } from "@/components/common/modals";
@@ -16,6 +17,12 @@ interface Props {
   children: ReactNode;
 }
 
+const poppins = Poppins({
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
+
 export default async function Layout({ children }: Props) {
   const response = await getLinks({ collections: "header,footer" });
   let links: UserLink[] = [];
@@ -26,7 +33,7 @@ export default async function Layout({ children }: Props) {
 
   return (
     <html lang="en">
-      <body className="container mx-auto px-8 md:px-16 lg:px-32">
+      <body className={`container mx-auto px-8 md:px-16 lg:px-32 ${poppins.className}`}>
         <ModalProvider>
           <Header links={links} />
           {children}
