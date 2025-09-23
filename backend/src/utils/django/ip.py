9a88d852b7b32cee72d79475bdaaab7697f119a6
CrazyProger1 <crazyproger1@gmail.com>
2025-09-23 10:02:56 +0300

    fix: get_client_ip

--- backend/src/utils/django/ip.py
+++ backend/src/utils/django/ip.py
@@ -2,5 +2,4 @@ from rest_framework.request import Request
 
 
 def get_client_ip(request: Request) -> str:
-    print(request.META)
-    return request.META.get("X-Forwarded-For", request.META.get("X-Real-IP", request.META.get("REMOTE_ADDR")))
+    return request.META.get("HTTP_X_FORWARDED_FOR", request.META.get("X-Real-IP", request.META.get("REMOTE_ADDR")))
