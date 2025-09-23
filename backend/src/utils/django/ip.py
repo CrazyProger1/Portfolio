8838bf33fdc60d76fb6fd15948b0a713c3b83cf3
CrazyProger1 <crazyproger1@gmail.com>
2025-09-23 10:00:47 +0300

    add: logs for get_client_ip

--- backend/src/utils/django/ip.py
+++ backend/src/utils/django/ip.py
@@ -2,4 +2,5 @@ from rest_framework.request import Request
 
 
 def get_client_ip(request: Request) -> str:
+    print(request.META)
     return request.META.get("X-Forwarded-For", request.META.get("X-Real-IP", request.META.get("REMOTE_ADDR")))
