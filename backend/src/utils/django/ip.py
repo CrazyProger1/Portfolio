26af2aa1c9331a9829a67710fa37d1a641871d2f
CrazyProger1 <crazyproger1@gmail.com>
2025-09-23 10:07:09 +0300

    add: fucking logs for get_client_ip

--- backend/src/utils/django/ip.py
+++ backend/src/utils/django/ip.py
@@ -4,6 +4,7 @@ from rest_framework.request import Request
 def get_client_ip(request: Request) -> str:
     ip = request.META.get("HTTP_X_FORWARDED_FOR", request.META.get("HTTP_X_REAL_IP", request.META.get("REMOTE_ADDR")))
 
+    print(f"IP ADDR: {ip}")
     if "," in ip:
         return ip.split(ip, maxsplit=1)[0]
     return ip
