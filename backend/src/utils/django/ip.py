5e5a0ac63c05756d73dfaed32c3e81efd46cbb99
CrazyProger1 <crazyproger1@gmail.com>
2025-09-23 10:10:57 +0300

    add: get_client_ip

--- backend/src/utils/django/ip.py
+++ backend/src/utils/django/ip.py
@@ -4,7 +4,6 @@ from rest_framework.request import Request
 def get_client_ip(request: Request) -> str:
     ip = request.META.get("HTTP_X_FORWARDED_FOR", request.META.get("HTTP_X_REAL_IP", request.META.get("REMOTE_ADDR")))
 
-    print(f"IP ADDR: {ip}")
     if "," in ip:
-        return ip.split(ip, maxsplit=1)[0]
+        return ip.split(",", maxsplit=1)[0]
     return ip
