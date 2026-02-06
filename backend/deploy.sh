3df9c5df59723a44635fcd0a7ddd2f133b2d95a1
Nikolay <crazyproger1@gmail.com>
2026-02-06 10:09:45 +0000

    feat: docker deploy

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -8,8 +8,4 @@ git checkout -- deploy.sh
 
 git pull
 
-#poetry run python manage.py makemigrations
-uv run python manage.py migrate
-uv run python manage.py collectstatic --noinput
-
-supervisorctl restart portfolio-backend
+docker compose up --build --detach
